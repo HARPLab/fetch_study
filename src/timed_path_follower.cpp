a3bfc093e79676704c1d051186c94ffb34926366
Ada Taylor <ada@idu.com>
2024-03-10 21:03:16 -0400

    fix to tomsg

--- src/timed_path_follower.cpp
+++ src/timed_path_follower.cpp
@@ -429,8 +429,8 @@ namespace path_executer
       tf2::Transform first_p;
       tf2::Transform second_p;
 
-      first_p   = tf2::convert(first);
-      second_p  = tf2::convert(second);
+      first_p   = tf2::toMsg(first);
+      second_p  = tf2::toMsg(second);
 
       //calculate the transformation between both poses (difference between both)
       tf2::Transform diff = first_p.inverse() * second_p;
