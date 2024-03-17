82a1371a97b298cffffafb1c29804a30b22f70f1
Ada Taylor <ada@idu.com>
2024-03-17 14:04:57 -0400

    Fix to frames

--- scripts/load_path.py
+++ scripts/load_path.py
@@ -20,7 +20,7 @@ class PathManager():
         #### Set up the node
         rospy.init_node('waypoints_loader')
         self.waypoint_pub = rospy.Publisher('/waypoints', Path, queue_size=1)
-        self.frame_id = rospy.get_param('~goal_frame_id', 'map')
+        self.goal_frame_id   = rospy.get_param('~goal_frame_id', 'map')
         self.odom_frame_id = rospy.get_param('~odom_frame_id', 'odom')
         self.base_frame_id = rospy.get_param('~base_frame_id', 'base_link')
 
@@ -118,8 +118,12 @@ class PathManager():
         try:
             while not rospy.is_shutdown() and not at_goal:
                 now = rospy.Time.now()
-                self.listener.waitForTransform(self.odom_frame_id, self.base_frame_id, now, rospy.Duration(4))
-                trans, rot = self.listener.lookupTransform(self.odom_frame_id, self.base_frame_id, now)
+            
+                self.listener.waitForTransform(self.base_frame_id, self.goal_frame_id, now, rospy.Duration(4))
+                trans, rot = self.listener.lookupTransform(self.base_frame_id, self.goal_frame_id, now)
+                
+                # self.listener.waitForTransform(self.odom_frame_id, self.base_frame_id, now, rospy.Duration(4))
+                # trans, rot = self.listener.lookupTransform(self.odom_frame_id, self.base_frame_id, now)
                 distance = np.sqrt(
                     pow(pgoal[0] - trans[0], 2) 
                         + pow(pgoal[1] - trans[1], 2))
