d74fb98d2f732ee615c230bdb8d7e2135e32c912
Ada Taylor <ada@idu.com>
2024-03-18 14:18:18 -0400

    Show format of goal dict

--- scripts/waypoints_pp.py
+++ scripts/waypoints_pp.py
@@ -167,6 +167,7 @@ class FollowRoute(State):
             tic = time.perf_counter()
 
             megapoint, megaroute_name = megatarget
+            print(goal_dict.values())
             px, py              = goal_dict[megaroute_name]
             path_to_broadcast   = route_dict[megaroute_name]
             # TODO verify this is good for first and last paths
