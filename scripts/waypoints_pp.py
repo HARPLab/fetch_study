260d7cd42decab5072633a5e02e54d259e0b5239
Ada Taylor <ada@idu.com>
2024-03-21 17:00:35 -0400

    publish path more often

--- scripts/waypoints_pp.py
+++ scripts/waypoints_pp.py
@@ -49,6 +49,8 @@ AUX_VELOCITY        = 1
 waypoint_pub        = None
 cmd_vel_publisher   = None
 
+FLAG_PP_MODE        = True
+
 # change Pose to the correct frame
 def changePose(waypoint, target_frame):
     if waypoint.header.frame_id == target_frame:
@@ -257,15 +259,17 @@ class FollowRoute(State):
 
 
                 if self.already_aligned_with_start_pose and not self.has_reached_endgoal:
-                    if not self.has_broadcast_curve:
-                        # self.waypoint_pub.publish(path_to_broadcast)
-                        self.client.send_goal(end_goal, done_cb=end_callback_done)
-                        rospy.loginfo('Executing move_base goal to END position (x,y) with velocity: %s, %s, %s' % (gx, gy, -1))
+                    # if not self.has_broadcast_curve:
+                    #     # self.waypoint_pub.publish(path_to_broadcast)
+                    #     self.client.send_goal(end_goal, done_cb=end_callback_done)
+                    #     rospy.loginfo('Executing move_base goal to END position (x,y) with velocity: %s, %s, %s' % (gx, gy, -1))
 
-                        self.has_broadcast_curve = True
-                        # else:
-                        #     blank_path = Path()
-                        #     # self.waypoint_pub.publish(blank_path)
+                    #     self.has_broadcast_curve = True
+                    #     # else:
+                    #     #     blank_path = Path()
+                    #     #     # self.waypoint_pub.publish(blank_path)
+
+                    self.waypoint_pub.publish(path_to_broadcast)
 
                     now = rospy.Time.now()
                     # self.listener.waitForTransform('map', 'base_link', now, rospy.Duration(4))
