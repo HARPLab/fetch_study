7bd0f3c4964e929200265f398ec671f8b3458430
Ada Taylor <ada@idu.com>
2024-05-03 14:53:55 -0400

    run command

--- scripts/waypoints_pid.py
+++ scripts/waypoints_pid.py
@@ -253,7 +253,7 @@ def get_exp_sequence(exp_option):
 
     elif exp_option == "p4a":
         return p4a
-    elif exp_option == "p4a":
+    elif exp_option == "p4b":
         return p4b
     elif exp_option == "p5a":
         return p5a
