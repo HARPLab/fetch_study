f907241e69928bd751c9ca06a389c108d922b0f4
Ada Taylor <ada@idu.com>
2024-05-22 13:37:01 -0400

    missng

--- scripts/waypoints_pid.py
+++ scripts/waypoints_pid.py
@@ -218,11 +218,11 @@ def get_exp_sequence(exp_option):
 
 
 
-    p4a = ['AD-null', 'DF_OBS-early', 'FC-late', 'CF-early', 'FA-early', 'AC-late', 'CB-early', 'BF-late', 'FE-late', 'EC-early', 'CA_OBS-late', 'AF-early', 'FB-early', 'BA-late', 'AE-late', 'ED-late', 'DF-late', 'FB-early', 'BE-late']
+    p4a = ['AD-null', 'DF_OBS-early', 'FB-early', 'BA-late', 'AE-late', 'ED-late', 'DF-late', 'FB-early', 'BE-late']
 
     p4b = ['AD-null', 'DF_OBS-late', 'FC-early', 'CF-late', 'FA-late', 'AC-early', 'CB-late', 'BF-early', 'FE-early', 'EC-late', 'CA_OBS-early', 'AF-late', 'FB-late', 'BA-early', 'AE-early', 'ED-early', 'DF-early', 'FB-late', 'BE-early']
 
-    p5a = ['EF-null', 'FD-early', 'DF_OBS-early', 'FA-late', 'FA-early', 'FD-late', 'DC-early', 'CA_OBS-late', 'AB-early', 'BF-late', 'FA-early', 'AE-late', 'EB-late', 'BD-late', 'DB-early', 'FC-late', 'CE-late', 'EC-early', 'CB-early', 'BC-late', 'CA-late', 'AD-early', 'DF_OBS-early', 'FE-late', 'EF-late', 'FD-late', 'DC-early', 'CA_OBS-late', 'AB-early', 'BF-late', 'FA-early', 'AE-late', 'EB-late', 'BD-late', 'DB-early']
+    p5a = ['EF-null', 'FC-late', 'CE-late', 'EC-early', 'CB-early', 'BC-late', 'CA-late', 'AD-early', 'DF_OBS-early', 'FE-late', 'EF-late', 'FD-late', 'DC-early', 'CA_OBS-late', 'AB-early', 'BF-late', 'FA-early', 'AE-late', 'EB-late', 'BD-late', 'DB-early']
 
     p5b = ['EF-null', 'FC-early', 'CE-early', 'EC-late', 'CB-late', 'BC-early', 'CA-early', 'AD-late', 'DF_OBS-late', 'FE-early', 'EF-early', 'FD-early', 'DC-late', 'CA_OBS-early', 'AB-late', 'BF-early', 'FA-late', 'AE-early', 'EB-early', 'BD-early', 'DB-late']
 
